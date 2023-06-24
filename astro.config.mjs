21e9f5df09a8e743a79bb3e595d99b7e1a9e7431
mohammedahmed18 <mohammed18200118@gmail.com>
2023-06-24 13:02:39 +0300

    homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), image(), tailwind(), sitemap()]
 });
